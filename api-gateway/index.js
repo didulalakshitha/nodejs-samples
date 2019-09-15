4089d24dfc280263a214ee66a47c27ef94255f40
Didula Dissanayake <didula.dissanayake@auxenta.com>
2019-09-15 23:07:41 +0530

    Added common url validation to avoid authentication

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express()
-const port = 7000
+const port = 7001
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -22,6 +22,13 @@ const serviceHosts = [
   },
 ];
 
+const commonUrls = [
+  {
+    'method': 'get',
+    'url': '/host-service/listofcooks',
+  },
+];
+
 const getMappingUrl = (requestPath) => {
   const requestPathSplitted = requestPath.split('/');
   let requestPathSlittedSecondItem = requestPathSplitted.length && requestPathSplitted.length > 1 ? requestPathSplitted[1] : null;
@@ -34,6 +41,19 @@ const getMappingUrl = (requestPath) => {
     }
   }
   return null;
+};
+
+const isCommonUrl = (requestPath, requestMethod) => {
+  if (!requestPath) {
+    return false;
+  }
+
+  requestPath = requestPath.toLowerCase();
+  const filteredUrls = commonUrls.filter(item => (requestPath.indexOf(item.url) > -1 && requestMethod.toLowerCase() === item.method));
+  if (filteredUrls.length) {
+    return true;
+  }
+  return false;
 };
 
 const accessAxios = async(req, res, config) => {
@@ -49,7 +69,9 @@ const accessAxios = async(req, res, config) => {
 router.use(async (req, res, next) => {
   console.log('%s %s %s - ', req.method, req.url, req.path, JSON.stringify(req.body, null, 2), req.header('content-type'));
   const mappingUrl = getMappingUrl(req.path);
-  if (req.url && mappingUrl) {
+
+  const isValidApi = isCommonUrl(req.path, req.method);
+  if (isValidApi && req.url &&  mappingUrl) {
     next();
   } else {
     res.status(500).send({
